268946fd8e315881c43ad9379fba9c312310104f
SuminJN <wjstnals1211@gmail.com>
2023-08-10 14:57:53 +0900

    Initial Commit

---
+++ Swiftui-examples/Swiftui_examplesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swiftui_examplesApp.swift
+//  Swiftui-examples
+//
+//  Created by 전수민 on 2023/08/10.
+//
+
+import SwiftUI
+
+@main
+struct Swiftui_examplesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
